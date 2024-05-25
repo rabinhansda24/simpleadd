6babc5e2b6928f6e26c08c65ba1cdf0c3016db3a
Rabin Hansda <rabinhansda24@gmail.com>
2024-05-25 19:50:10 +0530

    Add unit test for single number input in calc module

--- test_calc.py
+++ test_calc.py
@@ -5,6 +5,10 @@ class TestCalc(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(calc.add(''), 0)
 
+    def test_single_number(self):
+        self.assertEqual(calc.add('5'), 5)
+        self.assertEqual(calc.add('10'), 10)
+
 
 
 if __name__ == '__main__':
