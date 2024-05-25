48820cee70ada5e2e5d3eb43adcf00a9ace402a5
Rabin Hansda <rabinhansda24@gmail.com>
2024-05-25 20:03:38 +0530

    chore: Refactor add function to handle custom delimiters and new lines in numbers input

--- calc.py
+++ calc.py
@@ -1,8 +1,18 @@
+import re
+
 def add(numbers):
     if not numbers:
         return 0
     
-    numbers = [int(n) for n in numbers.split(',')]
+    #habdle new lines and custom delimiters
+    if numbers.startswith('//'):
+        delimiter, numbers = numbers.split('\n', 1)
+        delimiter = delimiter[2:]
+        numbers = re.split(re.escape(delimiter) + r'|\n', numbers)
+    else:
+        numbers = re.split(r',|\n', numbers)
+    
+    numbers = list(map(int, numbers))
 
     #check for negative numbers
     if any(n < 0 for n in numbers):
