58678ed74e60db9dd312a844b058c354e79e031b
Rabin Hansda <rabinhansda24@gmail.com>
2024-05-25 19:51:12 +0530

    Add unit test for two numbers input in calc module

--- test_calc.py
+++ test_calc.py
@@ -8,6 +8,10 @@ class TestCalc(unittest.TestCase):
     def test_single_number(self):
         self.assertEqual(calc.add('5'), 5)
         self.assertEqual(calc.add('10'), 10)
+    
+    def test_two_numbers(self):
+        self.assertEqual(calc.add('1,2'), 3)
+        self.assertEqual(calc.add('10,20'), 30)
 
 
 
